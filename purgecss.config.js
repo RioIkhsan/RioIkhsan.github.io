3f7a0efc45a140ff024c6ddf6408b4749e5f76e0
Rio Ikhsan <rioikhsan@Rios-MacBook-Air.local>
2023-09-24 16:19:19 +0700

    initial commit

---
+++ purgecss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "src/index.html",
+    "src/about/index.html",
+    "src/contact/index.html",
+    "src/portfolio/index.html",
+    "src/services/index.html",
+    "src/services/pembuatan-website/index.html",
+    "src/services/digital-marketing/index.html",
+    "src/services/desain-grafis/index.html",
+    "src/services/implementasi-erp/index.html",
+    "src/services/pengembangan-game/index.html",
+  ],
+  css: ["src/assets/css/main.css"],
+};
